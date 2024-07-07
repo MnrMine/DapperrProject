aba89b545f99fde4684ddcdd1f9877a3e6f4867c
MnrMine <mnr_mine_agri@hotmail.com>
2024-07-07 12:27:07 +0300

    Add project files.

---
+++ Dapperr/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using Dapperr.Dtos.CategoryDtos;
+
+namespace Dapperr.Services
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task DeleteCategoryAsync(int id);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task<GetByIdCategoryDto> GetCategoryAsync(int id);
+    }
+}
